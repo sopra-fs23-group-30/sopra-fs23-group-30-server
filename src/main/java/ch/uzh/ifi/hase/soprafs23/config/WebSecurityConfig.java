32eafb6e22e9f7abc111042d37597a3092c923d8
restermann2000 <97169812+restermann2000@users.noreply.github.com>
2023-04-10 16:20:25 +0200

    added condition so preflight gets through

--- src/main/java/ch/uzh/ifi/hase/soprafs23/config/WebSecurityConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/config/WebSecurityConfig.java
@@ -51,7 +51,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		// We don't need CSRF for this example
-		httpSecurity.csrf().disable()
+		httpSecurity.csrf().disable().cors().and()
 				// dont authenticate this particular request
 				.authorizeRequests().antMatchers("/").permitAll().and()
 				.authorizeRequests().antMatchers("/login").permitAll().and().authorizeRequests()
