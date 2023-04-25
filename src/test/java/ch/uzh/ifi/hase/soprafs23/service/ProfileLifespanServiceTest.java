ec778ef18e4e9251f4e26d0a527bd76bd3d0b180
make2002 <ms.kern@gmx.ch>
2023-04-25 17:50:29 +0200

    Update ProfileLifespanServiceTest.java

--- src/test/java/ch/uzh/ifi/hase/soprafs23/service/ProfileLifespanServiceTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/service/ProfileLifespanServiceTest.java
@@ -1,14 +1,21 @@
 package ch.uzh.ifi.hase.soprafs23.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import ch.uzh.ifi.hase.soprafs23.entity.ProfileEntity;
+import ch.uzh.ifi.hase.soprafs23.entity.ProfileLifespanEntity;
 import ch.uzh.ifi.hase.soprafs23.repository.ProfileLifespanRepository;
 import ch.uzh.ifi.hase.soprafs23.repository.ProfileRepository;
 
@@ -23,6 +30,7 @@ public class ProfileLifespanServiceTest {
     @InjectMocks
     private ProfileLifespanService profileLifespanService;
 
+    private ProfileLifespanEntity testProfileLifespanEntity;
     private ProfileEntity testProfile;
 
     @BeforeEach
@@ -30,7 +38,7 @@ public class ProfileLifespanServiceTest {
         MockitoAnnotations.openMocks(this);
 
         testProfile = new ProfileEntity();
-        testProfile.setId(new UUID(0, 0));
+        testProfile.setId(UUID.randomUUID());
         testProfile.setFirstname("test");
         testProfile.setLastname("name");
         testProfile.setEmail("test.name@example.com");
@@ -38,7 +46,47 @@ public class ProfileLifespanServiceTest {
         testProfile.setPassword("OneTwoThree");
         testProfile.setIsSearcher(false);
 
-        Mockito.when(profileRepository.save(Mockito.any())).thenReturn(testProfile);
+        testProfileLifespanEntity = new ProfileLifespanEntity();
+        // testProfileLifespanEntity.setId(UUID.randomUUID());
+        testProfileLifespanEntity.setProfile(testProfile);
+        testProfileLifespanEntity.setIsExperience(true);
+        testProfileLifespanEntity.setText("Truly amazing experiences");
+        testProfileLifespanEntity.setFromDate(Date.valueOf("1848-02-17"));
+        testProfileLifespanEntity.setToDate(Date.valueOf("2002-12-26"));
     }
 
+    @Test
+    void getAllLifespansByProfileId_existingId_success() {
+
+        testProfile = new ProfileEntity();
+        testProfile.setId(UUID.randomUUID());
+        testProfile.setFirstname("test");
+        testProfile.setLastname("name");
+        testProfile.setEmail("test.name@example.com");
+        testProfile.setPhoneNumber("+41 79 123 45 67");
+        testProfile.setPassword("OneTwoThree");
+        testProfile.setIsSearcher(false);
+
+        testProfileLifespanEntity = new ProfileLifespanEntity();
+        // testProfileLifespanEntity.setId(UUID.randomUUID());
+        testProfileLifespanEntity.setProfile(testProfile);
+        testProfileLifespanEntity.setIsExperience(true);
+        testProfileLifespanEntity.setText("Truly amazing experiences");
+        testProfileLifespanEntity.setFromDate(Date.valueOf("1848-02-17"));
+        testProfileLifespanEntity.setToDate(Date.valueOf("2002-12-26"));
+
+        List<ProfileLifespanEntity> testProfileLifespanEntities = new ArrayList<>();
+        testProfileLifespanEntities.add(testProfileLifespanEntity);
+
+        Mockito.when(profileLifespanRepository.findByProfileId(testProfile.getId()))
+                .thenReturn(testProfileLifespanEntities);
+
+        ProfileLifespanEntity found = profileLifespanService.getAllLifespansByProfileId(testProfile.getId()).get(0);
+
+        assertEquals(testProfileLifespanEntity.getProfile(), found.getProfile());
+        assertEquals(testProfileLifespanEntity.getIsExperience(), found.getIsExperience());
+        assertEquals(testProfileLifespanEntity.getText(), found.getText());
+        assertEquals(testProfileLifespanEntity.getFromDate(), found.getFromDate());
+        assertEquals(testProfileLifespanEntity.getToDate(), found.getToDate());
+    }
 }
