7f5365cab9d4b90cda99bb8b51dbae8e6d965a83
Mika Schoch <98819916+ThePunisher77777@users.noreply.github.com>
2023-04-26 21:19:59 +0200

    Again fixing NullPointerException handlings

--- src/main/java/ch/uzh/ifi/hase/soprafs23/service/BlobUploaderService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/service/BlobUploaderService.java
@@ -68,7 +68,7 @@ public class BlobUploaderService {
 
         List<String> imageUrls = new ArrayList<>();
         for (MultipartFile file : files) {
-            if (!file.getOriginalFilename().equals("")) {
+            if (!"".equals(file.getOriginalFilename())) {
                 String filename = generateUUID();
                 String imageUrl = upload(file, listingId, filename);
                 imageUrls.add(imageUrl);
