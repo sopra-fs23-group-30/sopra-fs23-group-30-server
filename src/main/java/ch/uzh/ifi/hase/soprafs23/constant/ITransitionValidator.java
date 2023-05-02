e1178c38364fd838e8bb45159d18264c66756e4d
make2002 <ms.kern@gmx.ch>
2023-05-03 00:33:42 +0200

    Added some more state transition functionality

--- src/main/java/ch/uzh/ifi/hase/soprafs23/constant/ITransitionValidator.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/constant/ITransitionValidator.java
@@ -1,5 +1,5 @@
 package ch.uzh.ifi.hase.soprafs23.constant;
 
 public interface ITransitionValidator {
-    public boolean validate(ApplicationState applicationState);
+    public boolean validate(ApplicationState applicationState, Boolean isLister);
 }
