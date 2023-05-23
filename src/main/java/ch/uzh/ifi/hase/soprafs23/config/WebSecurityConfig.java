db110e40578f2402c27d7e878aea54e2ec832b10
rafaaaaaaa <rafi_estermann@hotmail.ch>
2023-05-23 12:19:20 +0200

    another try

--- src/main/java/ch/uzh/ifi/hase/soprafs23/config/WebSecurityConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package ch.uzh.ifi.hase.soprafs23.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -23,11 +24,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
-	@Autowired
+
 	private UserDetailsService jwtUserDetailsService;
 
 	@Autowired
 	private JwtRequestFilter jwtRequestFilter;
+
+	@Autowired
+	public WebSecurityConfig(@Lazy UserDetailsService service) {
+		jwtUserDetailsService = service;
+	}
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
