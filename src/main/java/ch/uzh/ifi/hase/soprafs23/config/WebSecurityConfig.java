bfe45721e5fe1bf5e5e6eb8d9efd264ce2c4d570
rafaaaaaaa <rafi_estermann@hotmail.ch>
2023-05-23 18:21:11 +0200

    try fix

--- src/main/java/ch/uzh/ifi/hase/soprafs23/config/WebSecurityConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/config/WebSecurityConfig.java
@@ -16,60 +16,56 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	// @Autowired
-	// private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+	@Autowired
+	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
-	// @Autowired 
-	// @Lazy
-	// private UserDetailsService jwtUserDetailsService;
+	@Autowired 
+	@Lazy
+	private UserDetailsService jwtUserDetailsService;
 
-	// @Autowired
-	// private JwtRequestFilter jwtRequestFilter;
+	@Autowired
+	private JwtRequestFilter jwtRequestFilter;
 
-	// @Autowired
-	// public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-	// 	// configure AuthenticationManager so that it knows from where to load
-	// 	// user for matching credentials
-	// 	// Use BCryptPasswordEncoder
-	// 	auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
-	// }
+	@Override
+	public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+		authenticationManagerBuilder.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
+	}
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
-	// @Bean
-	// @Override
-	// public AuthenticationManager authenticationManagerBean() throws Exception {
-	// 	return super.authenticationManagerBean();
-	// }
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
 
-	// @Override
-	// protected void configure(HttpSecurity httpSecurity) throws Exception {
-	// 	// We don't need CSRF for this example
-	// 	httpSecurity.csrf().disable().cors().and()		
-	// 			// dont authenticate this particular request
-	// 			.authorizeRequests().antMatchers("/").permitAll().and()
-	// 			.authorizeRequests().antMatchers("/login").permitAll().and().authorizeRequests()
-	// 			.antMatchers("/registration").permitAll().and()
-	// 			.authorizeRequests().antMatchers("/listingpreviews").permitAll()
-	// 			.antMatchers("/ws*").permitAll()
-	// 			.antMatchers("/wss*").permitAll()
-	// 			// all other requests need to be authenticated
-	// 			.anyRequest().authenticated().and()
-	// 			// make sure we use stateless session; session won't be used to
-	// 			// store user's state.
-	// 			.exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
-	// 			.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		// We don't need CSRF for this example
+		httpSecurity.csrf().disable().cors().and()		
+				// dont authenticate this particular request
+				.authorizeRequests().antMatchers("/").permitAll().and()
+				.authorizeRequests().antMatchers("/login").permitAll().and().authorizeRequests()
+				.antMatchers("/registration").permitAll().and()
+				.authorizeRequests().antMatchers("/listingpreviews").permitAll()
+				.antMatchers("/ws*").permitAll()
+				.antMatchers("/wss*").permitAll()
+				// all other requests need to be authenticated
+				.anyRequest().authenticated().and()
+				// make sure we use stateless session; session won't be used to
+				// store user's state.
+				.exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-	// 	// Add a filter to validate the tokens with every request
-	// 	httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-	// }
+		// Add a filter to validate the tokens with every request
+		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+	}
 }
